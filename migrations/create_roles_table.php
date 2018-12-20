aacb6b13f7a2d7c7e1fb6d9d7a5de9746b133848
Zakhar Shchipets <zshchipets@gmail.com>
2018-12-21 01:53:31 +0300

    Update create_roles_table.php

--- migrations/create_roles_table.php
+++ migrations/create_roles_table.php
@@ -1,17 +1,24 @@
 <?php
 
-  public function up()
-  {
-    Schema::create('roles', function (Blueprint $table) {
-      $table->increments('id');
-      $table->string('name', 255);
-      $table->timestamps();
-    });
-  }
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-  public function down()
-  {
-    Schema::dropIfExists('roles');
-  }
+class CreateRolesTable extends Migration
+{
 
-?>
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+
+}
